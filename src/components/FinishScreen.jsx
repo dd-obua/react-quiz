6e591e2f97bbbaea16d2dc01a7ed0bac8cfa51cd
Obua Denis Daniel <dd.obua>
2024-05-09 20:15:01 +0300

    Fix decimal places

--- src/components/FinishScreen.jsx
+++ src/components/FinishScreen.jsx
@@ -2,7 +2,8 @@ const FinishScreen = function ({ points, maxPoints }) {
   const percentage = (points / maxPoints) * 100;
   return (
     <p className="result">
-      You scored <strong>{points}</strong> out of {maxPoints} ({percentage}% )
+      You scored <strong>{points}</strong> out of {maxPoints} (
+      {percentage.toFixed(2)}% )
     </p>
   );
 };
