0521f12673a73e935ec80dad8b43f71cd218f094
Obua Denis Daniel <dd.obua>
2024-05-09 17:39:18 +0300

    Style start button

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -3,7 +3,9 @@ const StartScreen = function ({ numQns, dispatch }) {
     <div className="start">
       <h2>Welcome to React Quiz</h2>
       <h3>{numQns} questions to test your react master</h3>
-      <button onClick={() => dispatch({ type: "start" })}>Let's start</button>
+      <button className="btn" onClick={() => dispatch({ type: "start" })}>
+        Let's start
+      </button>
     </div>
   );
 };
