3509da4c360e85417554c2aa31a960a3d6e137fb
Obua Denis Daniel <dd.obua>
2024-05-09 16:59:17 +0300

    Mange state using useReducer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,37 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import { useReducer } from "react";
+
+const intialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "",
+};
+
+const reducer = function (state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Action unknown");
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, intialState);
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './components/App';
-import './css/styles.css';
+import App from "./components/App";
+import "./css/styles.css";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
