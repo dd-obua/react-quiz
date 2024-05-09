0e9a30c8750d13e8cd3fc761925f3484bf98c1b7
Obua Denis Daniel <dd.obua>
2024-05-09 20:40:08 +0300

    Include emoji to finish message

--- src/components/FinishScreen.jsx
+++ src/components/FinishScreen.jsx
@@ -1,9 +1,16 @@
 const FinishScreen = function ({ points, maxPoints }) {
   const percentage = (points / maxPoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🏅";
+  if (percentage >= 80 && percentage < 100) emoji = "🥳";
+  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage >= 0 && percentage < 50) emoji = "🤔";
+  if (percentage === 0) emoji = "🤦‍♂️";
+
   return (
     <p className="result">
-      You scored <strong>{points}</strong> out of {maxPoints} (
-      {percentage.toFixed(2)}% )
+      <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+      {maxPoints} ({percentage.toFixed(2)}% )
     </p>
   );
 };
