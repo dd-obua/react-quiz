345705d188e06bb9b7b602fbb9cdda1b20d2474f
Obua Denis Daniel <dd.obua>
2024-05-09 18:32:27 +0300

    Lock clicked option and show correct answer

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,12 +1,15 @@
 const Options = function ({ qn, dispatch, answer }) {
+  const hasAnswer = answer !== null;
+
   return (
     <div>
       {qn.options.map((option, index) => (
         <button
-          className={`btn btn-option ${index === answer && "answer"} ${
-            answer === qn.correctOption ? "correct" : "wrong"
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswer ? (index === qn.correctOption ? "correct" : "wrong") : ""
           }`}
           key={option}
+          disabled={hasAnswer}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
           {option}
