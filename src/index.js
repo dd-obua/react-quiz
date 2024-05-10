9d03d80e357947cba2ff6a06fc91e9f8eaf99089
Obua Denis Daniel <dd.obua>
2024-05-10 18:36:05 +0300

    Use QuizProvider in index.js file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 
 import App from "./components/App";
 import "./css/styles.css";
+import { QuizProvider } from "./contexts/QuizContext";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <QuizProvider>
+      <App />
+    </QuizProvider>
   </StrictMode>
 );
